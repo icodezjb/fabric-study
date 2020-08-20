62bcb2713e98e10ee88387086750fbd436272192
icodezjb <icodezjb@163.com>
2020-08-20 19:04:31 +0800

    update files

---
+++ utils/replacepath.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+)
+
+const pkgName = "github.com/icodezjb/fabric-test"
+
+// ReplacePathInFile modify the path in config.yaml, replace `/absolute/path` by path.Join(goPath, "src", pkgName)
+func ReplacePathInFile(config string) []byte {
+	input, err := ioutil.ReadFile(config)
+	if err != nil {
+		log.Fatalln("ReadFile err:", err)
+	}
+
+	goPath := os.Getenv("GOPATH")
+
+	fmt.Println("first-network in:", path.Join(goPath, "src", pkgName))
+	return bytes.ReplaceAll(input, []byte("/absolute/path"), []byte(path.Join(goPath, "src", pkgName)))
+}
